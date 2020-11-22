dddee69293b38b479389f162a192293ca597ce71
Alex <alex26do@gmail.com>
2020-11-22 17:51:13 -0500

    Added database setup

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var dotenv = require('dotenv');
+var mongoose = require("mongoose");
+
+dotenv.config();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+// Database Setup
+var dbUrl = process.env.DB_URL || "mongodb://localhost/sprintu";
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(dbUrl, { 
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to database");
+}).catch(err => {
+    console.log("ERROR: ", err.message);
+})
 
 app.get("/", function(req, res){
     res.render("landing");
