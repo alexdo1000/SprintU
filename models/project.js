f82ef115a105930e9f9f0a037f4cd5d740174628
Alex <alex26do@gmail.com>
2020-12-03 18:56:23 -0500

    Added project model and updated user and board models

---
+++ models/project.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose");
+
+// SCHEMA SETUP
+var projectSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    owner: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        username: String,
+    },
+    members: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+});
+
+module.exports = mongoose.model("Project", projectSchema);
